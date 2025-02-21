57aa9c37ce4b61565085caf2f6951943d0a2dd6e
OwenBurt <122581502+OwenBurt@users.noreply.github.com>
2025-02-20 19:41:13 -0500

    xml_to_csv.py is ready

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -6,24 +6,57 @@ References: I referenced Chatura Wijetunga's object detection project as en exam
 Sources: Chatura Wijetunga: https://medium.com/nerd-for-tech/building-an-object-detector-in-tensorflow-using-bounding-box-regression-2bc13992973f
 '''
 
-import numpy as np
 import pandas as pd
 import glob
 import xml.etree.ElementTree as ET
 
-#Importing .png and .XML files from annotated_images folder.
-files = glob.glob('annotated_images/*')
+#This function handles annotated images
+def parse_annotated_files(files, df):
+    for i in range(1, len(files), 2):
+        contents = (ET.parse(files[1])).getroot()
+        file_name = contents.find('filename').text 
+        obj_class = contents.find('name').text
+        x1 = contents.find('xmin').text
+        y1 = contents.find('ymin').text
+        x2 = contents.find('xmax').text
+        y2 = contents.find('ymax').text
+        df = df.append({'file_name':file_name, 'obj_class':obj_class, 'x1':x1, 'y1':y1, 'x2':x2, 'y2':y2})
+    return df
 
-#Parsing out xml and storing it in dataframe.
-for i in range(1, len(files), 2):
-    contents = (ET.parse(files[1])).getroot()
-    img_name = contents.find('filename').text
-    img_height = contents.find('height').text
-    img_width = contents.find('width').text
-    img_name = contents.find('name').text
-    x1 = contents.find('xmin').text
-    y1 = contents.find('ymin').text
-    x2 = contents.find('xmax').text
-    y2 = contents.find('ymax').text
+#This function handles negtive images
+def negative_images(files, df):
+    for i in files:
+        file_name = i
+        obj_class = "NEGATIVE"
+        x1 = 0
+        y1 = 0
+        x2 = 0
+        y2 = 0
+    df = df.append({'file_name':file_name, 'obj_class':obj_class, 'x1':x1, 'y1':y1, 'x2':x2, 'y2':y2})
 
+
+#Importing .png and .XML files from annotated_images folder
+annotated_img_files = glob.glob('annotated_images/*')
+
+#Parsing out xml and storing it in dataframe
+annotated_img_df = pd.DataFrame()
+
+#Importing .png files from negative_images folder
+negative_img_files = glob.glob('negative_images/*')
+
+#Adding the negative image data to a dataframe
+negative_img_df = pd.DataFrame()
+
+#calling functions
+try:
+    annotated_df = parse_annotated_files(annotated_img_files, annotated_img_df)
+    print("Annotated images successful")
+except:
+    print("Error converting annotated images")
+
+try:
+    negative_df = negative_images(negative_img_files, negative_img_df)
+    print("Negative images successful")
+except:
+    print("Error adding negative files")
 
